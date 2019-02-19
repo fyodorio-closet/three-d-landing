04c50ff925b058e54e3b1323ef58c93a28dbe195
loenko <loenko.js@gmail.com>
2019-02-19 19:58:35 +0500

    Init repo, set scene and basic objects

---
+++ app/app.js
@@ -0,0 +1,16 @@
+import './app.scss';
+
+export const app = `
+<div class="background">
+    <div class="positioning-container">
+    <div class="bottom-padding-container">
+        <div class="base">
+            <div class="front"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+            <div class="back"></div>
+        </div>
+    </div>
+    </div>
+</div>
+`;
